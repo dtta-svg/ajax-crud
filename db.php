e93c3cce269d0ed605669a1f2509dddb71652c47
dtta-svg <75193083+dtta-svg@users.noreply.github.com>
2021-05-26 13:28:56 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,6 +2,6 @@
 
 $username = 'root';
 $password = 'SU%033910102%Man';
-$connection = new PDO( 'mysql:host=aa1ttzp6t00czfs.cfrhlbgs4h6o.ap-south-1.rds.amazonaws.com;dbname=test', $username, $password );
+$connection = new PDO( 'mysql:host=aa809rfe6d3xbm.c20kyxs9tred.ap-south-1.rds.amazonaws.com;dbname=test', $username, $password );
 
 ?>
