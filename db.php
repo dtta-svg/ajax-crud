6e40c1d30d9f18cdaf12ed6d29341718e85e683c
dtta-svg <sheab.dtta@gmail.com>
2024-05-23 01:16:41 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
 
-$username = 'wzrngrevfz';
-$password = 'RWXbdbtUv$gE5pMi';
-$connection = new PDO( 'mysql:host=my-frist-webasite-server.mysql.database.azure.com;dbname=dtta', $username, $password );
+// $username = 'wzrngrevfz';
+// $password = 'RWXbdbtUv$gE5pMi';
+// $connection = new PDO( 'mysql:host=my-frist-webasite-server.mysql.database.azure.com;dbname=dtta', $username, $password );
 
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, NULL, NULL);
+mysqli_real_connect($conn, "my-curd-app-server.mysql.database.azure.com", "djnuccgrnn", "m$laHeq$COiK2MLP", "dtta", 3306, MYSQLI_CLIENT_SSL);
+ 
 ?>
